42822cc99cfa9326dc701374b7faa1376443af02
DimDimShishkov <99988422+DimDimShishkov@users.noreply.github.com>
2022-08-01 19:31:11 +0300

    переименован файл api.js

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -87,7 +87,7 @@ class Api {
   }
 }
 
-const api = new Api({
+export const api = new Api({
   url: "https://mesto.nomoreparties.co/v1/cohort-45/",
   headers: {
     authorization: "02d68e11-bd2f-4860-a475-a40f4f8a5368",
